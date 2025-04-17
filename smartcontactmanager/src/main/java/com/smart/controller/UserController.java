38435f9996602b1b349e285e515430a099768c5c
Shaker <aishaker129@gmail.com>
2025-04-18 01:24:38 +0600

    working on user dashboard

--- smartcontactmanager/src/main/java/com/smart/controller/UserController.java
+++ smartcontactmanager/src/main/java/com/smart/controller/UserController.java
@@ -1,15 +1,33 @@
 package com.smart.controller;
 
+import java.security.Principal;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import com.smart.Dao.UserRepository;
+import com.smart.entities.User;
 
 @Controller
 @RequestMapping("/user")
 public class UserController {
+	
+	@Autowired
+	private UserRepository userRepository;
 
 	@GetMapping("/index")
-	public String dshbord() {
+	public String dshbord(Model m,Principal principal) {
+		String userName =  principal.getName();
+//		System.out.println("USER "+userName);
+		
+		// get the user using username(Email)
+		
+		User user = userRepository.getUserByUserName(userName);
+//		System.out.println("UserName "+user);
+		m.addAttribute("user",user);
 		return "Normal/user_dashbord";
 	}
 }
--- smartcontactmanager/src/main/resources/static/JS/script.js
+++ smartcontactmanager/src/main/resources/static/JS/script.js
@@ -1,1 +1,22 @@
 console.log("This is script file")
+
+const toggleSidebar = () => {
+	if($(".sidebar").is(":visible")){
+		$(".sidebar").css("display","none");
+		$(".content").css("margin-left","0%");
+	}
+	else{
+		$(".sidebar").css("display","block");
+		$(".content").css("margin-left","20%");
+	}
+};
+/*
+const toggleSidebar = () => {
+    $(".sidebar").toggle(); // toggles display
+    if ($(".sidebar").is(":visible")) {
+        $(".content").css("margin-left", "20%");
+    } else {
+        $(".content").css("margin-left", "0%");
+    }
+};*/
+
