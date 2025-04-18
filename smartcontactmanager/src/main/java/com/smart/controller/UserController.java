80cd6139f0a9f58c5d7d90a123de4340e2d090d8
Shaker <aishaker129@gmail.com>
2025-04-19 02:21:08 +0600

    working contact add page

--- smartcontactmanager/src/main/java/com/smart/controller/UserController.java
+++ smartcontactmanager/src/main/java/com/smart/controller/UserController.java
@@ -6,9 +6,11 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import com.smart.Dao.UserRepository;
+import com.smart.entities.Contact;
 import com.smart.entities.User;
 
 @Controller
@@ -17,10 +19,11 @@ public class UserController {
 	
 	@Autowired
 	private UserRepository userRepository;
-
-	@GetMapping("/index")
-	public String dshbord(Model m,Principal principal) {
-		String userName =  principal.getName();
+	
+	// method for adding common data to response
+	@ModelAttribute
+	public void addCommonData(Model m,Principal p) {
+		String userName =  p.getName();
 //		System.out.println("USER "+userName);
 		
 		// get the user using username(Email)
@@ -28,6 +31,24 @@ public class UserController {
 		User user = userRepository.getUserByUserName(userName);
 //		System.out.println("UserName "+user);
 		m.addAttribute("user",user);
+	}
+
+	// dashboard home
+	@GetMapping("/index")
+	public String dshbord(Model m,Principal principal) {
+		m.addAttribute("title","User Dashboard");
+		
 		return "Normal/user_dashbord";
+	}
+	
+	// open add form handler
+	@GetMapping("/add-contact")
+	public String openAddContactForm(Model m) {
+		
+		m.addAttribute("title","Add Contact");
+		m.addAttribute("contact",new Contact());
+		
+		
+		return "Normal/add_contact_form";
 	}
 }
